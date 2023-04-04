9f18f4657c41d8cccf58f6c5a8a34c72ae09c7f7
MargaridaTeles <telesmaegarida@gmail.com>
2023-04-04 16:40:04 +0100

    Fix Lampada

--- PuzzleLampadas/Lampada.cs
+++ PuzzleLampadas/Lampada.cs
@@ -2,14 +2,13 @@ using System;
 
 namespace PuzzleLampadas
 {
+    enum LampadaEstado{
+        Off, On
+    }
     public class Lampada
     {
         public LampadaEstado Estado {get; private set;} = LampadaEstado.Off;
 
         public string Nome {get; private set;}
-    }
-
-    public enum LampadaEstado{
-        Off, On
     }
 }
