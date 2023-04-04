90808adc6279012bc13937429904519922ef9a74
MargaridaTeles <telesmaegarida@gmail.com>
2023-04-04 16:51:47 +0100

    First Commit

--- PuzzleLampadas/Program.cs
+++ PuzzleLampadas/Program.cs
@@ -3,6 +3,13 @@
 
 namespace PuzzleLampadas
 {
+    public enum LampadaEstado{
+        Off, On
+    }
+    public class Lampada{
+        public LampadaEstado Estado {get; private set;} = LampadaEstado.Off;
+        public string Nome {get; private set;}
+    }
     class Program
     {
         static void Main(string[] args)
