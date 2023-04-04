243b5b303e0107e1a56562b2e95124b6af0c93e3
MargaridaTeles <telesmaegarida@gmail.com>
2023-04-04 16:21:11 +0100

    Criei Lampadas e Botões e limitei as chances disponiveis

--- PuzzleLampadas/Program.cs
+++ PuzzleLampadas/Program.cs
@@ -6,7 +6,24 @@ namespace PuzzleLampadas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* Existem 3 lampadas e 3 botões
+               O primeiro botão altera o estado da primeira lampada,
+               o segundo botão o estado da primeira e da segunda e
+               o terceiro o estado da segunda e da terceira. */
+
+            Lampada[] lampadas = {new Lampada("Lampada 1"), 
+                                new Lampada("Lampada 2"),
+                                new Lampada("Lampada 3")};
+
+            Botao[] botoes = {new Botao(lampadas[0]),
+                            new Botao(lampadas[0], lampadas[1]),
+                            new Botao(lampadas[1], lampadas[2])};
+
+            int maxpress = 6;
+            for (int i = 0; i < maxpress; i++)
+            {
+                Console.WriteLine($"{i+1} Chance");
+            }
         }
     }
 }
