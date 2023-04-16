b12f14ec3571dc073508175abc414eb6b12134d1
MargaridaTeles <telesmaegarida@gmail.com>
2023-04-16 15:30:25 +0100

    Corrigi o código de acordo com o mail da prof

--- PuzzleLampadas/Program.cs
+++ PuzzleLampadas/Program.cs
@@ -1,50 +1,92 @@
 ﻿using System;
-
+using System.Linq;
+using LampadaEstado;
 
 namespace PuzzleLampadas
 {
-    public enum LampadaEstado{
-        Off, On
-    }
-    public class Lampada{
-        public LampadaEstado Estado {get; private set;} = LampadaEstado.Off;
-        public string Nome {get; private set;}
-
-        public Lampada(string nome) => Nome = nome;
-    }
     class Program
     {
+        public static lampadas LampadasLigadas = 0;
+        
         static void Main(string[] args)
         {
             /* Existem 3 lampadas e 3 botões
                O primeiro botão altera o estado da primeira lampada,
-               o segundo botão o estado da primeira e da segunda e
-               o terceiro o estado da segunda e da terceira. */
+               o segundo botão o estado da primeira e da segunda (se ambos forem diferentes) e
+               o terceiro o estado da segunda e da terceira. (se ambos forem diferentes)
+               
+                Objetivo: Ligar todas as lâmpadas apenas carregando
+                no máximo 6 vezes nos botões.
+            */
+            string[] Lampadas = {"Lampada Esquerda", "Lampada Meio", "Lampada Direita"};
+            string[] Botoes = {"1", "2", "3"};
 
-            Lampada[] lampadas = {new Lampada("Lampada 1"), 
-                                new Lampada("Lampada 2"),
-                                new Lampada("Lampada 3")};
-
-            /*Botao[] botoes = {new Botao(lampadas[0]),
-                            new Botao(lampadas[0], lampadas[1]),
-                            new Botao(lampadas[1], lampadas[2])};*/
-
-            int maxpress = 6;
+            Console.WriteLine("\nObjetivo: Ligar todas as lâmpadas apenas carregando no máximo 6 vezes nos botões.");
+            int maxpress = 0;
             bool ganhou = false;
-            for (int i = 0; i < maxpress; i++)
-            {
-                Console.WriteLine($"{i+1} Chance");
-                ganhou = true;
 
-                foreach(Lampada lampada in lampadas)
+            while (maxpress < 6)
+            {
+                Console.WriteLine($"\n--- {maxpress+1}º Chance ---\n");
+                for (int k = 0; k < 3; k++)
                 {
-                    Console.WriteLine($"Lampada({lampada}) ");
-                    if(lampada.Estado == LampadaEstado.Off)
+                    Console.Write($"{Lampadas[k]}       ");
+                }
+                Console.WriteLine("");
+                for(int j = 0; j < 3; j++)
+                {   
+                    Console.Write($"    Botão {Botoes[j]}           ");
+                }
+                Console.Write("\n\nEscolha um botão de 1 a 3: ");
+                string input = Console.ReadLine();
+                while(!(input == Botoes[0] || input == Botoes[1] || input == Botoes[2]))
+                {
+                    Console.Write("Por favor escolha um botão de 1 a 3: ");
+                    input = Console.ReadLine();
+                }
+                if(input == "1")
+                {
+                    LampadasLigadas ^= lampadas.Lampada_esquerda;
+                }
+                if(input == "2")
+                {
+                    if(new[] {0, 6}.Contains((int)(LampadasLigadas & (lampadas.Lampada_meio | lampadas.Lampada_esquerda))))
                     {
-                        ganhou  = false;
+                        Console.WriteLine("Ligou Esquerda e Meio!");
+                    }
+                    else
+                    {
+                        LampadasLigadas ^= lampadas.Lampada_esquerda;
+                        LampadasLigadas ^= lampadas.Lampada_meio;
                     }
                 }
+                if(input == "3")
+                {
+                    if(new[] {0, 3}.Contains((int)(LampadasLigadas &(lampadas.Lampada_meio | lampadas.Lampada_direita))))
+                    {
+                        Console.WriteLine("Ligou Meio e Direita!");
+                    }
+                    else
+                    {
+                        LampadasLigadas ^= lampadas.Lampada_meio;
+                        LampadasLigadas ^= lampadas.Lampada_direita;
+                    }
+                }
+                
+                Console.WriteLine($"Ligou: {LampadasLigadas}");
+                maxpress++;
+            }
+            
+            if( (int)LampadasLigadas == 3 && maxpress <= 6 )
+            {
+                ganhou = true;
+                Console.WriteLine($"\nPUZZLE RESOLVIDO!! Em {maxpress} carregamentos!");
+            }
+            else
+            {
+                Console.WriteLine("\n\nPERDEU!");
             }
         }
+        
     }
 }
